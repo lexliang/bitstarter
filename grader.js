234ec2181f327f481da248af4bd5a3f56c8b2864
Ubuntu <ubuntu@ip-172-31-36-108.us-west-2.compute.internal>
2013-07-21 17:42:13 +0000

    Added grader.js

--- grader.js
+++ grader.js
@@ -21,10 +21,12 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKFILE_DEFAULT = "checks.json";
+var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,11 +41,11 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = finction(checksfile) {
+var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile. checksfile) {
+var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -64,10 +66,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url>', 'URL to html')
+	.parse(process.argv);
+    if (program.url)
+	{
+	    rest.get(program.url).on('complete', function(result);
+            var checkJson = checkHtmlFile(program.file, program.checks);
+	    console.log(result);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+	}
 } else {
+    var checkJson = checkHtmlFile(result, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
     exports.checkHtmlFile = checkHtmlFile;
 }
